9e67fdf5e0feb8434b4e5c07fb2f1f524656c594
harshal ganbote <harshal.ganbote@outlook.com>
2018-05-02 06:15:43 +0530

    error fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('My First Angular App!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to My First Angular App!');
   }));
 });
